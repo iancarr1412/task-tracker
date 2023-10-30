efbad14548ac88e5a33da59d54a8127dfbf80b67
iancarr1412 <iancarr1412@live.co.uk>
2023-10-30 18:57:50 +0000

    Props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
-const Header = () => {
-  return (
-    <header>
-        <h2>Test Tracker</h2>
-    </header>
-  )
-}
+import PropTypes from "prop-types";
+import Button from "./Button"
 
-export default Header
+const Header = ({ title, subTitle }) => {
+  const onClick = () => {
+    console.log("Click")
+  }
+  
+  return (
+    <header className="header">
+      <h2>{title}</h2>
+      <Button color="green" text="New Task" onClick={onClick}/>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Title from Default",
+  subTitle: "Track your work!",
+};
+
+// Typescript better option on prop types
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+};
+
+export default Header;
